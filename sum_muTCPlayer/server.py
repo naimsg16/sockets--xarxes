70538316b281dcdd849126efcc155c850bc9bf26
nsaadig16@gmail.com <naimsaadigallego@gmail.com>
2025-02-26 22:30:56 +0100

    fix tcp sum_multiplayer

--- sum_muTCPlayer/server.py
+++ sum_muTCPlayer/server.py
@@ -9,13 +9,13 @@ serv = socket.socket(
 serv.bind(("localhost",4320))
 serv.listen(2)
 time.sleep(15)
-serv.accept()
-serv.accept()
+
 
 sum = 0
 operators = 0
 while True:
-    data,client_addr = serv.recvfrom(10)
+    newsock = serv.accept()[0]
+    data,client_addr = newsock.recvfrom(10)
     int_data = int.from_bytes(data)
     sum += int.from_bytes(data)
     operators += 1
